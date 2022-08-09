434d55ccd4468e4ce12e6d09db073f55dbaea577
Cekap Yakup <61888196+cekapykp@users.noreply.github.com>
2022-08-10 02:13:46 +0300

    v4 - Blade.php nedir

--- example-app/resources/views/about.blade.php
+++ example-app/resources/views/about.blade.php
@@ -1,12 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>About</title>
 </head>
+
 <body>
     <h1>Bu sayfa about sayfasidir</h1>
+    <p>Hoşgeldin : {{ $name }}</p>
+    <?php
+    $sayi = 20;
+    ?>
+    @if ($sayi > 10)
+        <h4>Sayi değerimiz {{$sayi}}</h4>
+    @elseif($sayi == 15)
+        <h4>Sayi değerimiz 15</h4>
+    @else
+        <h4>Sayi değeri bilinmiyor</h4>
+    @endif
+
 </body>
+
 </html>
--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -16,9 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
 Route::get('/about',function(){
-    return view('about');
+    return view('about', ['name' => 'Cekap']);
 });
 
 Route::get('/user',function(){
