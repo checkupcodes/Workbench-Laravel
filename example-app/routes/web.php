9879775c39d3dd0fd4344a81d0acb1e3b4e5ed38
Cekap Yakup <61888196+cekapykp@users.noreply.github.com>
2022-08-10 02:05:41 +0300

    v3- MVC ve Routers

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::delete('/about', function () {
+Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/about',function(){
+    return view('about');
+});
+
+Route::get('/user',function(){
+    return "Merhaba user";
 });
