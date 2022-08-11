be82b94384f070d435e44d297c86ef6bc6e3e90e
Cekap Yakup <61888196+cekapykp@users.noreply.github.com>
2022-08-11 16:17:17 +0300

    v9 - Breeze konu anlatım

--- example-app/resources/views/dashboard.blade.php
+++ example-app/resources/views/dashboard.blade.php
@@ -9,7 +9,14 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    You're logged in!<br>
+                    {{Auth::user()->name}} <br>
+                    Email adresiniz : {{Auth::user()->email}} <br>
+                    Email şifre : {{Auth::user()->password}} <br>
+                    Email id : {{Auth::user()->id}} <br>
+                    Email token : {{Auth::user()->remember_token}} <br>
+                    Email oluşturma tarihi : {{Auth::user()->created_at}} <br>
+
                 </div>
             </div>
         </div>
