0ad09ae6ade5e8266f1fc70dc1731ea888db152a
Cekap Yakup <61888196+cekapykp@users.noreply.github.com>
2022-08-31 02:09:52 +0300

    v16- Admin Paneli - 3 - Segmentlere Ayırma

---
+++ laracheck/resources/views/admin/body/footer.blade.php
@@ -0,0 +1,6 @@
+<div class="footer">
+    <div class="copyright">
+        <p>Copyright © Designed &amp; Developed by <a href="https://dexignlab.com/"
+                target="_blank">DexignLab</a> 2021</p>
+    </div>
+</div>
