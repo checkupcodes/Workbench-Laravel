32ffd750f1aa5d8be0d9ddc62c72236813a32c43
Cekap Yakup <61888196+cekapykp@users.noreply.github.com>
2022-09-03 01:36:32 +0300

    v19 - Admin Panel - 6 - VerifiedMail

--- laracheck/routes/web.php
+++ laracheck/routes/web.php
@@ -24,6 +24,6 @@ Route::controller(AdminController::class)->group(function () {
 
 Route::get('/dashboard', function () {
     return view('admin.index');
-})->middleware(['auth'])->name('dashboard');
+})->middleware(['auth','verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';
