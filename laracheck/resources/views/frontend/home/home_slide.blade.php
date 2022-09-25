280c19bf8092f83a9d809f40e0275f2c96f32dc8
Cekap Yakup <61888196+cekapykp@users.noreply.github.com>
2022-09-25 17:36:00 +0300

    v40 - HomeSlide FrontEnd

---
+++ laracheck/resources/views/frontend/home/home_slide.blade.php
@@ -0,0 +1,32 @@
+@php
+    $homeSlide = App\Models\HomeSlide::find(1);
+@endphp
+
+<section class="banner">
+    <div class="container custom-container">
+        <div class="row align-items-center justify-content-center justify-content-lg-between">
+            <div class="col-lg-6 order-0 order-lg-2">
+                <div class="banner__img text-center text-xxl-end">
+                    <img src="{{$homeSlide->home_slide}}" alt="">
+                </div>
+            </div>
+            <div class="col-xl-5 col-lg-6">
+                <div class="banner__content">
+                    <h2 class="title wow fadeInUp" data-wow-delay=".2s">
+                       {{$homeSlide->title}}
+                    </h2>
+                    <p class="wow fadeInUp" data-wow-delay=".4s">
+                        {{$homeSlide->description}}
+                    </p>
+                    <a href="about.html" class="btn banner__btn wow fadeInUp" data-wow-delay=".6s">more about me</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="scroll__down">
+        <a href="#aboutSection" class="scroll__link">Scroll down</a>
+    </div>
+    <div class="banner__video">
+        <a href="{{$homeSlide->video_url}}" class="popup-video"><i class="fas fa-play"></i></a>
+
+</section>
--- laracheck/resources/views/frontend/index.blade.php
+++ laracheck/resources/views/frontend/index.blade.php
@@ -2,30 +2,7 @@
 @section('main')
 
     <!-- banner-area -->
-    <section class="banner">
-        <div class="container custom-container">
-            <div class="row align-items-center justify-content-center justify-content-lg-between">
-                <div class="col-lg-6 order-0 order-lg-2">
-                    <div class="banner__img text-center text-xxl-end">
-                        <img src="{{asset('frontend/assets/img')}}/banner/banner_img.png" alt="">
-                    </div>
-                </div>
-                <div class="col-xl-5 col-lg-6">
-                    <div class="banner__content">
-                        <h2 class="title wow fadeInUp" data-wow-delay=".2s"><span>I will give you Best</span> <br> Product in the shortest time.</h2>
-                        <p class="wow fadeInUp" data-wow-delay=".4s">I'm a Rasalina based product design & visual designer focused on crafting clean & user‑friendly experiences</p>
-                        <a href="about.html" class="btn banner__btn wow fadeInUp" data-wow-delay=".6s">more about me</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="scroll__down">
-            <a href="#aboutSection" class="scroll__link">Scroll down</a>
-        </div>
-        <div class="banner__video">
-            <a href="https://www.youtube.com/watch?v=XHOmBV4js_E" class="popup-video"><i class="fas fa-play"></i></a>
-       
-    </section>
+    @include('frontend.home.home_slide')
     <!-- banner-area-end -->
 
     <!-- about-area -->
